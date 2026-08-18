5c03ea88372eb38020c2bdccf18d9ecd685b9a5a
Shresth <shresth000777@gmail.com>
2026-08-18 16:38:03 +0530

    0009-PALINDROME-NUMBER Stats: Time: 4 ms (100%), Space: 45.8 MB (87.68%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,19 +1,18 @@
 class Solution {
     public boolean isPalindrome(int x) {
 
-        if (x < 0) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
 
-        int reverse = 0;
-        int xcopy = x;
+        int reversedHalf = 0;
 
-        while (x > 0) {
-            reverse = (reverse * 10) + (x % 10);
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
             x /= 10;
         }
 
-        return reverse == xcopy;        
+        return x == reversedHalf || x == reversedHalf / 10;
 
     }
 }
